9eb9968e3e806d63abad747442c41e1b5aa8d919
Anton Serdyuchenko <anton415@gmail.com>
2026-08-07 10:39:26 +0300

    lesson 2: add variables and named type

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+type MeetingID string
+
 func main() {
-	fmt.Println("MeetupLite started")
+	var title string = "Go Basics Meetup"
+	participantCount := 3
+	var isPublished bool
+	const maxParticipants int = 50
+	var meetingID MeetingID = "meetup-001"
+
+	fmt.Printf("title=%q, type=%T\n", title, title)
+	fmt.Printf("participantCount=%d, type=%T\n", participantCount, participantCount)
+	fmt.Printf("isPublished=%t, type=%T\n", isPublished, isPublished)
+	fmt.Printf("maxParticipants=%d, type=%T\n", maxParticipants, maxParticipants)
+	fmt.Printf("meetingID=%q, type=%T\n", meetingID, meetingID)
+
+	rawID := string(meetingID)
+	fmt.Println(rawID)
 }
